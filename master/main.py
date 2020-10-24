aa255e97ce74b8621154e7ccaee25d9039b62ab2
Frederick Ambo <frederick@tacaly.com>
2020-10-24 02:34:42 +0200

    Update main.py

--- master/main.py
+++ master/main.py
@@ -2,15 +2,11 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'FMS enabled: Version {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('0.0.1')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
